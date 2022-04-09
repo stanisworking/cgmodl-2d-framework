003a020534f6b43b3c570212ecfa15668f5170a5
Stan <stanley.haryoto@gmail.com>
2022-04-09 23:37:08 +0800

    Added script to MeshObject prefab
    MeshObject script is to generate the mesh data for the selected MeshType

--- CGMODL-2D-FRAMEWORK/Assets/Scripts/MeshGenerator.cs
+++ CGMODL-2D-FRAMEWORK/Assets/Scripts/MeshGenerator.cs
@@ -11,7 +11,17 @@ public struct MeshData
 
 public enum MeshType
 {
-    MESH_TRIANGLE = 0
+    MESH_TRIANGLE = 0,
+    MESH_QUAD,
+    MESH_PENTAGON,
+    MESH_HEXAGON,
+    MESH_HEPTAGON,
+    MESH_OCTAGON,
+    MESH_NANOGON,
+    MESH_DECAGON,
+    MESH_CIRCLE,
+    MESH_OVAL,
+    MESH_STAR
 }
 
 public class MeshGenerator
@@ -20,6 +30,36 @@ public class MeshGenerator
     {
         switch(meshType)
         {
+            //case MeshType.MESH_STAR:
+            //    return GenerateStar();
+
+            //case MeshType.MESH_OVAL:
+            //    return GenerateOval();
+
+            //case MeshType.MESH_CIRCLE:
+            //    return GenerateCircle();
+
+            //case MeshType.MESH_DECAGON:
+            //    return GenerateDecagon();
+
+            //case MeshType.MESH_NANOGON:
+            //    return GenerateNanogon();
+
+            //case MeshType.MESH_OCTAGON:
+            //    return GenerateOctagon();
+
+            //case MeshType.MESH_HEPTAGON:
+            //    return GenerateHeptagon();
+
+            //case MeshType.MESH_HEXAGON:
+            //    return GenerateHexagon();
+
+            //case MeshType.MESH_PENTAGON:
+            //    return GeneratePentagon();
+
+            //case MeshType.MESH_QUAD:
+            //    return GenerateQuad();
+
             case MeshType.MESH_TRIANGLE:
             default:
                 return GenerateTriangle();
@@ -29,6 +69,17 @@ public class MeshGenerator
     private static MeshData GenerateTriangle()
     {
         MeshData meshData = new MeshData();
+
+        // Todo: Add the vertices that makes up triangle
+        meshData.vertices = new Vector3[]
+        {
+        };
+
+        // Todo: Add the sequence of vertices to form the triangle
+        meshData.triangles = new int[] 
+        { 
+        };
+
         return meshData;
     }
 }
---
+++ CGMODL-2D-FRAMEWORK/Assets/Scripts/MeshObject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshObject : MonoBehaviour
+{
+    [SerializeField]
+    MeshType meshType;
+
+    Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        transform.position = new Vector3(0, 0, 0);
+
+        InitialiseMesh();
+    }
+
+    void InitialiseMesh()
+    {
+        mesh.Clear();
+
+        MeshData meshData = MeshGenerator.Generate(meshType);
+        mesh.vertices = meshData.vertices;
+        mesh.triangles = meshData.triangles;
+
+    }
+}
